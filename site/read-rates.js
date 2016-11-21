feaaf6cc9dca978a83a88cf617890fe1613f20b4
Don Vince <don@donvince.co.uk>
2016-11-21 17:07:10 +0000

    a little jslinting to keep the crock gods happy

--- site/read-rates.js
+++ site/read-rates.js
@@ -1,8 +1,8 @@
+"use strict";
 (function () {
     $.getJSON("http://api.fixer.io/latest", function (data) {
         fx.rates = data.rates;
         var rate = fx(1).from("GBP").to("USD");
         alert("£1 = $" + rate.toFixed(4));
     });
-})();
-
+}());
