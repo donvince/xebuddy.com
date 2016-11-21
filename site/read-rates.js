cacdac56d07dc574052694bb1f4e5eb1073d2e25
Don Vince <don@donvince.co.uk>
2016-11-21 18:53:01 +0000

    now generates table on the fly

--- site/read-rates.js
+++ site/read-rates.js
@@ -1,8 +1,16 @@
 "use strict";
 (function () {
+    var from = [5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000];
+
     $.getJSON("http://api.fixer.io/latest", function (data) {
         fx.rates = data.rates;
-        var rate = fx(1).from("GBP").to("USD");
-        alert("£1 = $" + rate.toFixed(4));
+
+        var tableBody = $("#data");
+        tableBody.empty();
+
+        for(var i = 0; i < from.length; i++) {
+            var to = fx(from[i]).from("HUF").to("GBP").toFixed(2);
+            tableBody.append("<tr><td><h2>" + from[i] + "</h2></td><td><h2>" + to + "</h2></td></tr>");
+        }
     });
 }());
