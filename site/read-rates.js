d35d5c9a8ffb41e5e0528049cba977bd02c6e146
Don Vince <don@donvince.co.uk>
2016-11-22 18:06:52 +0000

    formatted the output table that took longer than expected!

--- site/read-rates.js
+++ site/read-rates.js
@@ -2,6 +2,49 @@
 (function () {
     var from = [5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000];
 
+    var currencyIndex = [
+        {
+            "currency": "HUF",
+            "name": "Hungarian forint",
+            "format": "0,0$",
+            "language": "hu",
+            "denominations": [
+                5,
+                10,
+                20,
+                50,
+                100,
+                200,
+                500,
+                1000,
+                2000,
+                5000,
+                10000,
+                20000
+            ]
+        },
+        {
+            "currency": "GBP",
+            "name": "British Pound",
+            "format": "$0,0.00",
+            "language": "en-gb",
+            "denominations": [
+                0.1,
+                0.2,
+                0.5,
+                0.10,
+                0.20,
+                0.50,
+                1,
+                2,
+                5,
+                10,
+                20,
+                50
+            ]
+        }
+    ];
+
     var updateTable = function (){
         var tableBody = $("#data");
         tableBody.empty();
@@ -9,9 +52,28 @@
         var leftCurrency = $("#leftSelected").text();
         var rightCurrency = $("#rightSelected").text();
 
+        var leftCurrencySettings;
+        var rightCurrencySettings;
+
+        for(var currency of currencyIndex) {
+            if(currency.currency === leftCurrency) {
+                leftCurrencySettings = currency;
+            }
+            if(currency.currency === rightCurrency) {
+                rightCurrencySettings = currency;
+            }
+        }
+
         for(var i = 0; i < from.length; i++) {
             var to = fx(from[i]).from(leftCurrency).to(rightCurrency).toFixed(2);
-            tableBody.append("<tr><td>" + from[i] + "</td><td>" + to + "</td></tr>");
+
+            numeral.language(leftCurrencySettings.language);
+            var fromFormatted = numeral(from[i]).format(leftCurrencySettings.format);
+
+            numeral.language(rightCurrencySettings.language);
+            var toFormatted = numeral(to).format(rightCurrencySettings.format);
+
+            tableBody.append("<tr><td>" + fromFormatted + "</td><td>" + toFormatted + "</td></tr>");
         }
     };
 
