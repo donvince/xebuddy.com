813ea785a79fb8dbe506518a33666bb1b9ddc26e
Don Vince <don@donvince.co.uk>
2016-12-12 19:29:24 +0000

    Added US Dollar

--- site/read-rates.js
+++ site/read-rates.js
@@ -556,11 +556,26 @@
                 100,
                 200
             ]
+        },
+        {
+            "currency": "USD",
+            "name": "US Dollar",
+            "format": "$0,0.00",
+            "language": "en",
+            "denominations": [
+                0.01,
+                0.05,
+                0.10,
+                0.25,
+                1,
+                5,
+                10,
+                20,
+                50,
+                100
+            ]
         }
-
         // TODO:
-        // ""
-        // "USD"
         // "ZAR"
 
 
