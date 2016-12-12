7e63ebab351a58bca3116701174e4a52ae2bdfc2
Don Vince <don@donvince.co.uk>
2016-12-12 18:51:30 +0000

    Added Norwegian Krone

--- site/read-rates.js
+++ site/read-rates.js
@@ -380,11 +380,28 @@
                 50,
                 100,
             ]
+        },
+        {
+            "currency": "NOK",
+            "name": "Norwegian Krone",
+            "format": "0,0",
+            "language": "en",
+            "denominations": [
+                1,
+                5,
+                10,
+                20,
+                50,
+                100,
+                200,
+                500,
+                1000
+            ]
         }
+
+
         // TODO:
         // ""
-        // ""
-        // "NOK"
         // "NZD"
         // "PHP"
         // "PLN"
