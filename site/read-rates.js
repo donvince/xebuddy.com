6577b0c4842fc71576a822d63cd61aa3d15bd0fb
Don Vince <don@donvince.co.uk>
2016-12-12 19:13:02 +0000

    Added Swedish Krona

--- site/read-rates.js
+++ site/read-rates.js
@@ -489,13 +489,25 @@
                 1000,
                 5000
             ]
+        },
+        {
+            "currency": "SEK",
+            "name": "Swedish Krona",
+            "format": "0,0",
+            "language": "en",
+            "denominations": [
+                1,
+                5,
+                10,
+                20,
+                50,
+                100,
+                200,
+                500
+            ]
         }
-
         // TODO:
         // ""
-        // ""
-        // "RUB"
-        // "SEK"
         // "SGD"
         // "THB"
         // "TRY"
